4da4fd03f8f70e26283c80f8940d804a7d747d9b
liuqiang <liu1142865396@gmail.com>
2022-06-14 01:44:28 +0800

    feat: pay 模块完成

---
+++ spring-cloud-mulin-pay/src/main/java/com/liumulin/pay/PayApplication.java
@@ -0,0 +1,17 @@
+package com.liumulin.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 支付启动类
+ *
+ * @author liuqiang
+ * @since 2022-06-14
+ */
+@SpringBootApplication
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
